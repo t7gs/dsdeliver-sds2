ba6870da53c8dcfe91c63ee36179a7b3e86a022d
Tiago Rocha <tigs4dev@gmail.com>
2021-01-16 12:09:52 -0300

    GET /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
@@ -8,21 +8,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.dsdeliver.dto.ProductDTO;
-import com.devsuperior.dsdeliver.services.ProductService;
+import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.services.OrderService;
 
 @RestController
-@RequestMapping(value="/products")
-public class ProductController {
+@RequestMapping(value="/orders")
+public class OrderController {
 	
 	//injeção de dependencia
 	@Autowired
-	private ProductService service;
+	private OrderService service;
 	
 	//criando um endpoint
 	@GetMapping
-	public ResponseEntity<List<ProductDTO>> findAll(){
-		List <ProductDTO> list = service.findAll();
+	public ResponseEntity<List<OrderDTO>> findAll(){
+		List <OrderDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
